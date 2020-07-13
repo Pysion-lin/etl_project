1807336a68e055b81797b399641d64dcca051090
Pysion-lin <1450933795@qq.com>
2020-07-13 09:47:52 +0800

    修改增量型更新方式为时间范围,可以在Dev中设置

--- ETLSchedule/manage.py
+++ ETLSchedule/manage.py
@@ -2,31 +2,6 @@ from ETLSchedule.Schedule.workscheduler import ApSchedulerProcess
 from ETLSchedule.utils import UpdateResource,UpdateTaskScheduleStatus
 from ETLSchedule.task import task_queue
 
-# 程序入口函数
-# def main():
-#     try:
-#         # import models  # 初始化数据库连接
-#         setting_queue = dev.ALL_QUEUE
-#         scheduler = ApSchedulerProcess()  # 初始化调度器
-#         for path in setting_queue:
-#             module_path, class_name = path.get("job").rsplit('.', 1)
-#             modle = importlib.import_module(module_path)
-#             cls = getattr(modle, class_name)
-#             parameter = path.get("parameter")
-#             if not cls:
-#                 raise ValueError("任务不存在")
-#             run(scheduler,cls,parameter)
-#         scheduler.start()  # 启动调度器任务
-#     except ValueError as v:
-#         print(v.__str__())
-#     except Exception as e:
-#         traceback.print_exc()
-#
-#
-# # 添加任务作业
-# def run(scheduler,cls,parameter):
-#     scheduler.add_job(cls,**parameter)
-
 
 def main():
 
